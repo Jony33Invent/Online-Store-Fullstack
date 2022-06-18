de6eefbc62edc6d0463cfc5a45864462ddf8ca70
PedroRaymundi <62019741+PedroRaymundi@users.noreply.github.com>
2022-06-18 15:33:40 -0300

    mudanças nas categorias
    *Adicionado cores;
    *Arrumada a jornada de usuário;

---
+++ parte2React/src/components/Categories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/style.css';
+import './styles/dropdown.css';
+
+function Categories() {
+    return (
+        <div>
+            <h1>Categorias</h1>
+        </div>
+    )
+}
+
+export default Categories
---
+++ parte2React/src/components/SectionBox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './styles/style.css';
+import ProductCard from './ProductCard';
+
+function SectionBox({section}) {
+    
+    let productCards = []
+    for (let i = 0; i < section.products.length; i++) {
+      productCards.push(<ProductCard product={section.products[i]}/>)
+    }   
+    return (
+        <div class="section box">
+            <div>
+            <h2>{section.title}</h2>
+            <p>{section.text}</p>
+            </div>
+            {productCards}
+        </div>
+    )
+}
+export default SectionBox
